0ed22f65b6f972707ea557efd7e06ea440978059
Anders Båtsman <anders@loghouse.dev>
2022-12-01 14:24:52 +0100

    Initial commit with template

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Advent of code 2022
+
+
+Running a solution
+```
+cargo run --release --bin day_01
+```
---
+++ src/template.rs
@@ -0,0 +1,14 @@
+fn part1() -> &'static str {
+    "Skeleton for part 1"
+}
+
+fn part2() -> &'static str {
+    "Skeleton for part 2"
+}
+
+fn main() {
+    println!("Results for template");
+    println!("-------------------");
+    println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2());
+}
